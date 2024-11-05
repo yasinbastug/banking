87795234c0dd5f7aadf91043bb54c3a8dd09f5a1
Kartik Sharma <Kartik.cityu@gmail.com>
2024-11-05 17:00:28 +0800

    Bank singleton class

--- src/main/java/com/bank/Main.java
+++ src/main/java/com/bank/Main.java
@@ -1,13 +1,21 @@
+
 package com.bank;
+
 import com.bank.models.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Get the singleton instance of the Bank
+        Bank bank = Bank.getInstance();
+
+        // Create an AccountService instance
         AccountService accountService = new AccountService();
 
-        // Create accounts
-        Account savingsAccount = new SavingsAccount("123456", "user1", 1000);
-        Account checkingAccount = new CheckingAccount("654321", "user1", 500);
+        // Create accounts for the client
+        Account savingsAccount = new SavingsAccount("123456", client.getUsername(), 1000);
+        Account checkingAccount = new CheckingAccount("654321", client.getUsername(), 500);
+        
+        // Open accounts in the account service
         accountService.openAccount(savingsAccount);
         accountService.openAccount(checkingAccount);
 
@@ -19,5 +27,11 @@ public class Main {
 
         // Transfer between accounts
         accountService.transfer("123456", "654321", 50);
+
+        // (Optional) Display all users in the bank
+        System.out.println("Users in the bank:");
+        for (User user : bank.getUsers()) {
+            System.out.println(user.getUsername() + " - " + user.getRole().getName());
+        }
     }
 }
---
+++ src/main/java/com/bank/auth/Bank.java
@@ -0,0 +1,43 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private static Bank instance; // Singleton instance
+    private List<User> users; // List to store users
+
+    // Private constructor to prevent instantiation
+    private Bank() {
+        users = new ArrayList<>();
+    }
+
+    // Method to get the singleton instance
+    public static synchronized Bank getInstance() {
+        if (instance == null) {
+            instance = new Bank();
+        }
+        return instance;
+    }
+
+    // Method to add a user
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    // Method to get all users
+    public List<User> getUsers() {
+        return new ArrayList<>(users); // Return a copy of the user list
+    }
+
+    // Method to find a user by username (or other criteria as needed)
+    public User findUserByUsername(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null; // User not found
+    }
+
+    // Additional methods for account management can be added here
+}
